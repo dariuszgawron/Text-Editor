f641374936931ca601501b4065efc883924c3b49
Dariusz Gawron <screbsy+g1@gmail.com>
2021-12-14 22:13:49 +0100

    Improve classes, styles and scripts

--- js/script.js
+++ js/script.js
@@ -1,36 +1,62 @@
 const textArea = document.querySelector('.content__editor');
 
-const copyBtn = document.querySelector('.toolbar__btn[name=copy]');
-const pasteBtn = document.querySelector('.toolbar__btn[name=paste]');
-const cutBtn = document.querySelector('.toolbar__btn[name=cut]');
+const undoBtn = document.querySelector('.toolbar-button[name=undo]');
+const redoBtn = document.querySelector('.toolbar-button[name=redo]');
 
-const nameBtn = document.querySelector('.toolbar__select[name=name]');
-const sizeBtn = document.querySelector('.toolbar__select[name=size]');
-const boldBtn = document.querySelector('.toolbar__btn[name=bold]');
-const italicBtn = document.querySelector('.toolbar__btn[name=italic]');
-const underlineBtn = document.querySelector('.toolbar__btn[name=underline]');
-const strikeBtn = document.querySelector('.toolbar__btn[name=strike]');
-const subscriptBtn = document.querySelector('.toolbar__btn[name=subscript]');
-const superscriptBtn = document.querySelector('.toolbar__btn[name=superscript]');
-const colorBtn = document.querySelector('.toolbar__btn[name=color]');
+const copyBtn = document.querySelector('.toolbar-button[name=copy]');
+const pasteBtn = document.querySelector('.toolbar-button[name=paste]');
+const cutBtn = document.querySelector('.toolbar-button[name=cut]');
 
-const outdentBtn = document.querySelector('.toolbar__btn[name=outdent]');
-const indentBtn = document.querySelector('.toolbar__btn[name=indent]');
-const alignLeftBtn = document.querySelector('.toolbar__btn[name=align-left]');
-const alignCenterBtn = document.querySelector('.toolbar__btn[name=align-center]');
-const alignRightBtn = document.querySelector('.toolbar__btn[name=align-right]');
-const alignJustifyBtn = document.querySelector('.toolbar__btn[name=align-justify]');
-const orderedListBtn = document.querySelector('.toolbar__btn[name=ordered-list]');
-const unorderedListBtn = document.querySelector('.toolbar__btn[name=unordered-list]');
+const nameBtn = document.querySelector('.toolbar-select[name=name]');
+const sizeBtn = document.querySelector('.toolbar-select[name=size]');
+const boldBtn = document.querySelector('.toolbar-button[name=bold]');
+const italicBtn = document.querySelector('.toolbar-button[name=italic]');
+const underlineBtn = document.querySelector('.toolbar-button[name=underline]');
+const strikeBtn = document.querySelector('.toolbar-button[name=strike]');
+const subscriptBtn = document.querySelector('.toolbar-button[name=subscript]');
+const superscriptBtn = document.querySelector('.toolbar-button[name=superscript]');
+const colorBtn = document.querySelector('.toolbar-button[name=color]');
 
-const removeFormatBtn = document.querySelector('.toolbar__btn[name=remove-format]');
+const outdentBtn = document.querySelector('.toolbar-button[name=outdent]');
+const indentBtn = document.querySelector('.toolbar-button[name=indent]');
+const alignLeftBtn = document.querySelector('.toolbar-button[name=align-left]');
+const alignCenterBtn = document.querySelector('.toolbar-button[name=align-center]');
+const alignRightBtn = document.querySelector('.toolbar-button[name=align-right]');
+const alignJustifyBtn = document.querySelector('.toolbar-button[name=align-justify]');
+const orderedListBtn = document.querySelector('.toolbar-button[name=ordered-list]');
+const unorderedListBtn = document.querySelector('.toolbar-button[name=unordered-list]');
+
+const removeFormatBtn = document.querySelector('.toolbar-button[name=remove-format]');
+
+const listOfFonts = ["Times New Roman","Arial","Georgia","Palatino","Calibri"];
 
 function changeText(commandName, defaultUI = false, valueArgument = null) {
     document.execCommand(commandName, defaultUI, valueArgument);
     textArea.focus();
 }
 
-window.onload = () => textArea.focus(); 
+function getStyleValue(element, propertyName) {
+    return window.getComputedStyle(element, null).getPropertyValue(propertyName);
+}
+
+function sortStringItems(a, b) {
+    return (a < b) ? -1 : (a > b) ? 1 : 0;
+}
+
+function addItemsToList(list, items, selectedItem) {
+    items.sort(sortStringItems).forEach(item => list.innerHTML += `<option class="toolbar-select__option" value="${item}" ${('"'+item+'"'===selectedItem) ? "selected" : ""}>${item}</option>`);
+}
+
+window.onload = () => {
+    // const detective = new Detector();
+    // detective.detect('font name');
+    const fontName = getStyleValue(textArea,'font-family');
+    addItemsToList(nameBtn,listOfFonts, fontName);
+    textArea.focus();
+} 
+
+undoBtn.addEventListener('click', () => changeText('undo'));
+redoBtn.addEventListener('click', () => changeText('redo'));
 
 copyBtn.addEventListener('click', () => changeText('copy'));
 pasteBtn.addEventListener('click', () => changeText('paste'));
